797aa067ea6cf8cedf15c2304dbb427735283358
LeelaRohith <leelarohith10@gmail.com>
2024-03-19 18:47:21 +0530

    redux store configured

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 -Sign up form
 -Form validation
 -useRef Hook
+-Implement signin and signup
+-Created redux store using user slice
+-Implementated Signout feature
+-Updated profile API call to store user name
--- src/components/Login.js
+++ src/components/Login.js
@@ -5,11 +5,17 @@ import { checkValidateData } from "../utils/validate";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  updateProfile,
 } from "firebase/auth";
 import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { addUser } from "../utils/userSlice";
+import { useDispatch } from "react-redux";
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const fullName = useRef(null);
   const email = useRef(null);
   const password = useRef(null);
@@ -31,7 +37,25 @@ const Login = () => {
         .then((userCredential) => {
           // Signed up
           const user = userCredential.user;
-          console.log(user);
+          updateProfile(user, {
+            displayName: fullName.current.value,
+            photoURL: "https://example.com/jane-q-user/profile.jpg",
+          })
+            .then(() => {
+              // Profile updated!
+              const { uid, email, displayName } = auth.currentUser;
+              dispatch(
+                addUser({ uid: uid, email: email, displayName: displayName })
+              );
+              // ...
+              navigate("/browse");
+            })
+            .catch((error) => {
+              // An error occurred
+              // ...
+            });
+          // console.log(user);
+
           // ...
         })
         .catch((error) => {
@@ -52,6 +76,7 @@ const Login = () => {
           // Signed in
           const user = userCredential.user;
           console.log(user);
+          navigate("/browse");
           // ...
         })
         .catch((error) => {
